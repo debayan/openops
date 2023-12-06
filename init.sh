2b8f79dd02dff9baa7db6b79f56ba68e5d3b83b9
demouser@hcdsgpu2 <demouser@hcdsgpu2>
2023-12-06 07:56:07 +0000

    Adding local modifications for docker compose files

--- init.sh
+++ init.sh
@@ -22,12 +22,12 @@ sleep 35
 
 echo -e "Setting up Mattermost with ...\n Team name: $team_name\n Team display name: $team_display_name\n Channel name: $channel_name\n Channel display name: $channel_display_name"
 
-docker exec mattermost mmctl --local team create --display-name $team_display_name --name $team_name
-docker exec mattermost mmctl --local channel create --team $team_name --display-name "$channel_display_name" --name $channel_name
+docker exec llmui-mattermost mmctl --local team create --display-name $team_display_name --name $team_name
+docker exec llmui-mattermost mmctl --local channel create --team $team_name --display-name "$channel_display_name" --name $channel_name
 
-docker exec mattermost mmctl --local user create --username $user_name --password $user_password --email $user_name@$team_name.com --system-admin --email-verified
-docker exec mattermost mmctl --local team users add $team_name $user_name
-docker exec mattermost mmctl --local channel users add $team_name:$channel_name $user_name
+docker exec llmui-mattermost mmctl --local user create --username $user_name --password $user_password --email $user_name@$team_name.com --system-admin --email-verified
+docker exec llmui-mattermost mmctl --local team users add $team_name $user_name
+docker exec llmui-mattermost mmctl --local channel users add $team_name:$channel_name $user_name
 
 export MM_ADMIN_USERNAME=$user_name
 export MM_ADMIN_PASSWORD=$user_password
@@ -36,16 +36,16 @@ export MM_SERVICESETTINGS_ENABLEDEVELOPER=true
 
 echo "Installing plugin."
 
-docker exec mattermost mmctl --local plugin install-url https://github.com/mattermost/mattermost-plugin-ai/releases/download/v$plugin_version/mattermost-ai-$plugin_version.tar.gz
-docker exec mattermost mmctl --local plugin enable mattermost-ai
+docker exec llmui-mattermost mmctl --local plugin install-url https://github.com/mattermost/mattermost-plugin-ai/releases/download/v$plugin_version/mattermost-ai-$plugin_version.tar.gz
+docker exec llmui-mattermost mmctl --local plugin enable mattermost-ai
 
 # Configure plugin
 if [ "$backend" == 'localai' ]; then
-	cat config_patch_localai.json | docker exec -i mattermost bash -c 'mmctl --local config patch /dev/stdin'
+	cat config_patch_localai.json | docker exec -i llmui-mattermost bash -c 'mmctl --local config patch /dev/stdin'
 fi
 
 if [ "$backend" == 'openai' ]; then
-	cat config_patch_openai.json | docker exec -i mattermost bash -c 'mmctl --local config patch /dev/stdin'
+	cat config_patch_openai.json | docker exec -i llmui-mattermost bash -c 'mmctl --local config patch /dev/stdin'
 fi
 
 
